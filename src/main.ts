6f90fc6f8ef77660f3e30ee436c3b4977ae776ab
Tiri <shaymusts@gmail.com>
2023-03-06 20:01:01 +0200

    added port

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,6 @@ async function bootstrap() {
     origin: "https://expense-tracker-client-three.vercel.app",
   });
   app.setGlobalPrefix("api");
-  await app.listen(5000);
+  await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
